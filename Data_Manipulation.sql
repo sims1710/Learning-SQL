ac1e848dcb6e263bd9826d573673025d2d33ef15
Simriti <simritib1@gmail.com>
2024-01-29 22:12:09 +0800

    Update Data_Manipulation.sql

--- Data_Manipulation.sql
+++ Data_Manipulation.sql
@@ -53,3 +53,30 @@ SET twitter_handle = '@taylorswift13'
 WHERE id = 4; 
 
 SELECT * FROM celebs;
+
+-- The DELETE FROM statement deletes one or more rows from a table. 
+DELETE FROM celebs 
+WHERE twitter_handle IS NULL;
+
+-- Constraints : They can be used to tell the database to reject inserted data that does not adhere to a certain restriction.
+CREATE TABLE celebs (
+   id INTEGER PRIMARY KEY, 
+   name TEXT UNIQUE,
+   date_of_birth TEXT NOT NULL,
+   date_of_death TEXT DEFAULT 'Not Applicable'
+);
+
+/*
+1. PRIMARY KEY columns can be used to uniquely identify the row. Attempts to insert a row with an 
+identical value to a row already in the table will result in a constraint violation which will not 
+allow you to insert the new row.
+
+2. UNIQUE columns have a different value for every row. This is similar to PRIMARY KEY except a table 
+can have many different UNIQUE columns.
+
+3. NOT NULL columns must have a value. Attempts to insert a row without a value for a NOT NULL column 
+will result in a constraint violation and the new row will not be inserted.
+
+4. DEFAULT columns take an additional argument that will be the assumed value for an inserted row if 
+the new row does not specify a value for that column.
+*/
