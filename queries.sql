a63c70043f0ed449ff5487ebe34e7d3ca1f793ef
Simriti <simritib1@gmail.com>
2024-02-07 13:40:16 +0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -40,3 +40,59 @@ pattern in a column.
 - The _ means you can substitute any individual character here without breaking
 the pattern. The names Seven and Se7en both match this pattern.
 */
+
+-- Like II
+
+/*
+The percentage sign % is another wildcard character that can be used with LIKE.
+
+This statement below filters the result set to only include movies with names 
+that begin with the letter ‘A’:
+*/
+SELECT * 
+FROM movies
+WHERE name LIKE 'A%';
+
+-- % is a wildcard character that matches zero or more missing characters in the pattern. For example:
+
+/*
+A% matches all movies with names that begin with letter ‘A’
+%a matches all movies that end with ‘a’
+We can also use % both before and after a pattern:
+*/
+
+SELECT * 
+FROM movies 
+WHERE name LIKE '%man%';
+
+-- Here, any movie that contains the word ‘man’ in its name will be returned in the result.
+
+-- Is Null
+-- To filter for all movies with an IMDb rating:
+
+SELECT name
+FROM movies 
+WHERE imdb_rating IS NOT NULL;
+
+-- BETWEEN
+/*
+The BETWEEN operator is used in a WHERE clause to filter the result set 
+within a certain range. It accepts two values that are either numbers, 
+text or dates.
+
+For example, this statement filters the result set to only include 
+movies with years from 1990 up to, and including 1999.
+*/
+
+SELECT *
+FROM movies
+WHERE year BETWEEN 1990 AND 1999;
+
+/*
+In this statement, BETWEEN filters the result set to only include movies with 
+names that begin with the letter ‘A’ up to, but not including ones that begin with ‘J’.
+*/
+
+SELECT *
+FROM movies
+WHERE name BETWEEN 'A' AND 'J';
