e98963962fa6093f9167c1b67863093893268214
Simriti <simritib1@gmail.com>
2024-02-10 17:40:32 +0800

    Update Multiple_Tables.sql

--- Multiple_Tables.sql
+++ Multiple_Tables.sql
@@ -4,4 +4,27 @@ FROM orders
 JOIN customers
   ON orders.customer_id = customers.customer_id;
 
--- INNER JOIN
+-- LEFT JOIN
+/*
+A left join will keep all rows from the first table, regardless 
+of whether there is a matching row in the second table.
+*/
+
+SELECT *
+FROM table1
+LEFT JOIN table2
+  ON table1.c2 = table2.c2;
+
+-- Primary Key vs Foreign Key
+/*
+Primary keys have a few requirements:
+- None of the values can be NULL.
+- Each value must be unique (i.e., you can’t have two customers 
+with the same customer_id in the customers table).
+- A table can not have more than one primary key column.
+
+When the primary key for one table appears in a different table, 
+it is called a foreign key.
+*/
+
+-- CROSS JOIN 
